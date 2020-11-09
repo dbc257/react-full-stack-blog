8b1763269763784db8e3ab45b302d85a8f719570
David Carlisle <davidcarlisle@pop-os.localdomain>
2020-11-09 16:09:09 -0600

    added more front-end with Material-UI

--- 2020-08-full-stack-react-blog-main/client/src/components/Posts.js
+++ 2020-08-full-stack-react-blog-main/client/src/components/Posts.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Header, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Button, Form, Header, Modal, Segment } from 'semantic-ui-react'
 
 export default function Posts() {
-
+  const [ formOpen, setFormOpen ] = useState(false);
   const [ posts, setPosts ] = useState([])
+  const [ title, setTitle ] = useState('');
+  const [ author, setAuthor ] = useState('');
+  const [ published, setPublished ] = useState('');
+  const [ content, setContent ] = useState('');
 
   // componentDidMount
   useEffect(() => {
@@ -13,6 +18,31 @@ export default function Posts() {
         setPosts(data);
       })
   }, [])
+
+  const handleFormSubmit = (e) => {
+    fetch('/api/v1/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        author: author,
+        title: title,
+        published: published,
+        content: content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setFormOpen(false);
+        setPosts(posts.concat(data));
+        setTitle('')
+        setAuthor('')
+        setPublished('')
+        setContent('')
+      })
+
+  }
 
   return (
     <div>
@@ -24,13 +54,31 @@ export default function Posts() {
               <Header as="h2">{ post.title }</Header>
               <Header size='small'>{ post.author }</Header>
               <p>{ post.content.slice(0, 200)}{ post.content.length > 200 && "..."}</p>
-              { post.content.length > 200 && (
-                <a href="#">Read More</a>
-              )}
+              <Link to={`/post/${post.id}`}>Read More</Link>
             </Segment>
           </div>
         }) }
       </Segment>
+      <Modal
+        trigger={<Button>Add New Post</Button>}
+        open={formOpen}
+        onOpen={() => setFormOpen(true)}
+        onClose={() => setFormOpen(false)}
+      >
+        <Modal.Header>Add a new Post</Modal.Header>
+        <Modal.Content>
+          <Form id="newPostForm" onSubmit={handleFormSubmit}>
+            <Form.Input required label="Title" type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+            <Form.Input required label="Author" type="text" value={author} onChange={(e) => { setAuthor(e.target.value) }} />
+            <Form.Input required label="Publish Date" type="datetime-local" value={published} onChange={(e) => { setPublished(e.target.value) }} />
+            <Form.TextArea required label="Content" value={content} onChange={(e) => { setContent(e.target.value) }} />
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={() => setFormOpen(false)}>Cancel</Button>
+          <Button positive form="newPostForm">Submit</Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   )
 }
